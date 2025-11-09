d28a5177e762a0141fdc3b1823b1105b2a44a2d0
Julian <julianl116910@gmail.com>
2025-11-09 14:37:32 -0500

    Inicializando con spring

---
+++ src/main/java/edu/dosw/rideci/TROYA_REPUTATION_BACKEND/TroyaReputationBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.TROYA_REPUTATION_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroyaReputationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroyaReputationBackendApplication.class, args);
+	}
+
+}
