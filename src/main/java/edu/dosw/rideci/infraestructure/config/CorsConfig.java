33e196dbb98262e659ecbba1ae13ce7a4b266ae0
Julian <julianl116910@gmail.com>
2025-12-03 18:21:29 -0500

    fix(core)- añadiendo cors config

--- src/main/java/edu/dosw/rideci/infraestructure/config/CorsConfig.java
+++ src/main/java/edu/dosw/rideci/infraestructure/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package edu.dosw.rideci.infrastructure.config;
+package edu.dosw.rideci.infraestructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
