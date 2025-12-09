f73d32a1e1510cfd5cbbd31053b474ca116514b4
Julian <julianl116910@gmail.com>
2025-12-08 21:09:08 -0500

    feat creacion de evento

--- src/main/java/edu/dosw/rideci/application/events/ProfileEvent.java
+++ src/main/java/edu/dosw/rideci/application/events/ProfileEvent.java
@@ -1,25 +1,41 @@
 package edu.dosw.rideci.application.events;
 
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import edu.dosw.rideci.domain.model.Badge;
+import edu.dosw.rideci.domain.model.Reputation;
+import edu.dosw.rideci.domain.model.Vehicle;
 import edu.dosw.rideci.domain.model.enums.IdentificationType;
+import edu.dosw.rideci.domain.model.enums.ProfileType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
-@Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProfileEvent {
-
+    
     private Long userId;
     private String name;
-    private String email;
-    private IdentificationType identificationType;
-    private String identificationNumber;
+    private List<Vehicle> vehicles;
+    private Reputation calification;
     private String phoneNumber;
+    private ProfileType profileType;
+    private List<Long> ratings;
+    private List<Badge> badges;
+    private String email;
+    private String identificationNumber;
     private String address;
-    private String role;
-    private String state;
+    private LocalDateTime birthDate;
+    private String profilePictureUrl;
+    private IdentificationType identificationType;
+    
 
 }
